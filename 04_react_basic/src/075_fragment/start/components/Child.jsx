d2a2b563caed8884b3278711da721a058a213620
“C.K” <dripcoke24@gmail.com>
2024-04-22 22:51:44 +0900

    Framgmentの勉強

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,20 @@
-import "./Child.css";
+import "./Child.css"
+import { Fragment } from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
+        asperiores cum placeat sequi quas veniam. Hic provident culpa delectus
+        rerum quisquam in quidem error ratione ipsam tenetur. Saepe, deleniti
+        velit.
+      </p>
+    </Fragment>
   );
 };
 
