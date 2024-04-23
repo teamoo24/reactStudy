b07474103fdc704204d29d450547bf38dff40387
“C.K” <dripcoke24@gmail.com>
2024-04-23 21:38:48 +0900

    propsに色々な値を渡してみよう

--- 04_react_basic/src/090_props/start/Example.jsx
+++ 04_react_basic/src/090_props/start/Example.jsx
@@ -1,12 +1,23 @@
 import Child from "./components/Child";
 
 const Example = () => {
-    return (
-        <>
-            <Child />
-            <Child color="red"/>
-        </>
-    )
-}
+  const hello = (arg) => `Hello ${arg}`;
+  const o = {
+    color: "red",
+    num: 123
+  }
+  return (
+    <>
+      {/* <Child color="red" /> */}
+      {/* <Child num={123} /> */}
+        <Child 
+        {...o}
+        fn={hello} 
+        bool 
+        obj={{ name: "Tom", age: "18" }} 
+        />
+    </>
+  );
+};
 
 export default Example;
