bdd07700ed1427f2b2890c75be18a751f20d3b5e
“C.K” <dripcoke24@gmail.com>
2024-04-24 12:27:28 +0900

    propsとchildrenの勉強

--- 04_react_basic/src/110_props_children/start/Example.jsx
+++ 04_react_basic/src/110_props_children/start/Example.jsx
@@ -2,14 +2,22 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container
+        title="Childrenとは？"
+        children={[
+          <Profile key={profile[0].name} {...profile[0]} />,
+          <Profile key={profile[1].name} {...profile[1]} />,
+        ]}
+        first={<Profile key={profile[0].name} {...profile[0]} />}
+        second={<Profile key={profile[1].name} {...profile[1]} />}
+      />
     </div>
   );
 };
