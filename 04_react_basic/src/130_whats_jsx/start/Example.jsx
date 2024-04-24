436a16dc5bcc3ddedb98fe0b1946dd702df71f5b
“C.K” <dripcoke24@gmail.com>
2024-04-24 20:25:41 +0900

    jsxとは何か？

--- 04_react_basic/src/130_whats_jsx/start/Example.jsx
+++ 04_react_basic/src/130_whats_jsx/start/Example.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Example = () => {
   const sample1 = <h1 className="greeting">Hello World</h1>;
 
-  // React.createElement("h1", {
-  //   className: "greeting"
-  // }, "Hello World");
+  console.log(sample1);
 
   const sample2 = (
     <div>
@@ -15,12 +13,29 @@ const Example = () => {
   );
 
   console.log(
-    (
-      <div>
-        <h1>Hello!</h1>
-        <h2>Good to see you.</h2>
-      </div>
-    ).props
+    React.createElement(
+      "h1",
+      {
+        className: "greeting",
+      },
+      "Hello World"
+    )
+  );
+
+  // console.log(
+  //   (
+  //     <div>
+  //       <h1>Hello!</h1>
+  //       <h2>Good to see you</h2>
+  //     </div>
+  //   ).props
+  // );
+
+  React.createElement(
+    "div",
+    null,
+    React.createElement("h1", null, "Hello!"),
+    React.createElement("h2", null, "Good to see you.")
   );
 
   return React.createElement(
