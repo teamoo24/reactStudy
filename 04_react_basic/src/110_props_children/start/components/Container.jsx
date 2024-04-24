e63c6e2c6f64dd54f5e2e7b46e656efaa5b35322
“C.K” <dripcoke24@gmail.com>
2024-04-24 17:16:08 +0900

    propsのルール学習

--- 04_react_basic/src/110_props_children/start/components/Container.jsx
+++ 04_react_basic/src/110_props_children/start/components/Container.jsx
@@ -3,11 +3,17 @@ import "./Container.css";
 const Container = ({ title, children, first, second }) => {
   console.log(children);
   return (
-    <div className="container">
-      <h3>{title}</h3>
-      {first}
-      {second}
-    </div>
+    <>
+      <div className="container">
+        <h3>{title}</h3>
+        {children}
+      </div>
+      <div className="container">
+        <h3>{title}</h3>
+        {second}
+        {first}
+      </div>
+    </>
   );
 };
 
--- 04_react_basic/src/120_props_rules/start/Example.jsx
+++ 04_react_basic/src/120_props_rules/start/Example.jsx
@@ -1,14 +1,17 @@
 // POINT propsの流れは一方通行
 // POINT propsは読み取り専用
 
-import Bye from "./components/Bye"
-import Hello from "./components/Hello"
+import Bye from "./components/Bye";
+import Hello from "./components/Hello";
 
 const Example = () => {
+  // POINT propsの流れは一方通行
+  const name = "Tom";
+
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={name} />
+      <Bye name={name} />
     </>
   );
 };
