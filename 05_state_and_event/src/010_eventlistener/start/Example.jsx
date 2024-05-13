7a2ec40110260bdcc05bb7bb1fb7e4840e7c1be6
“C.K” <dripcoke24@gmail.com>
2024-05-14 08:15:32 +0900

    イベントに合わせて関数を実行してみよう

--- 05_state_and_event/src/010_eventlistener/start/Example.jsx
+++ 05_state_and_event/src/010_eventlistener/start/Example.jsx
@@ -1,6 +1,15 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert("ボタンがクリックされました");
+  };
+  const clickHandler2 = () => {
+    console.log("ボタンがクリックされました。");
+  };
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button onClick={clickHandler2}>クリックしてね</button>
+    </>
   );
 };
 
